667b8a9af4ae46754e03d95b4a8909053ee6626d
Eric Sergueev <eric.sergueev@gmail.com>
2023-10-30 21:04:24 +0100

    feat : add total article counter

--- src/Components/Store/Header.jsx
+++ src/Components/Store/Header.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { getNumberProduct } from "../Cart/cart";
+
 
 export function Header() {
   return (
@@ -6,8 +8,10 @@ export function Header() {
       <div className="container mx-auto flex justify-between items-center">
         <Link to={'/'} className="text-2xl font-bold">Ma Boutique</Link>
         <nav>
-        <Link to={'/'} className="ml-4">Accueil</Link>
-          <Link to={'/cart'} className="ml-4">Panier</Link>
+          <Link to={'/'} className="ml-4">Accueil</Link>
+          <Link to={'/cart'} className="ml-4">Panier 
+          <span className="bg-red-500 rounded-full py-1 px-3 ">{getNumberProduct()}</span>
+          </Link>
           <a href="#" className="ml-4">Connexion</a>
         </nav>
       </div>

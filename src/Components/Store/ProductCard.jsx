68f6e3a6d4d4eeb41c114beccad63f2436c5a46a
Eric Sergueev <eric.sergueev@gmail.com>
2023-10-31 09:42:43 +0100

    feat : add bouton to delete category selection

--- src/Components/Store/ProductCard.jsx
+++
@@ -1,7 +0,0 @@
-export function ProductCard() {
-    return (
-      <div className="w-3/4 grid grid-cols-3 gap-4">
-        
-      </div>
-    );
-  }
--- src/Components/Store/Sidebar.jsx
+++ src/Components/Store/Sidebar.jsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export function Sidebar(props) {
-  const { categorys } = props
+  const { categorys } = props;
+  const [searchParams, setSearchParams] = useSearchParams();
+    const searchCategory = searchParams.get('category');
 
   return (
     <div className="w-1/4">
@@ -10,6 +12,7 @@ export function Sidebar(props) {
         {categorys.map(category =>
           <li key={category} className="mb-2">
             <Link to={`/?category=${category}`} className="text-blue-500 hover:underline">{category}</Link>
+            {category == searchCategory ? (<Link to={'/'} className="bg-red-500 rounded-full px-3 py-1 ml-5 text-white">X</Link>):(<></>)}
           </li>)}
       </ul>
     </div>

474c530d7cce50e977bacb97f5917a5a32481c36
Eric Sergueev <eric.sergueev@gmail.com>
2023-11-02 11:14:33 +0100

    style: responsive home page

--- src/Components/Store/Sidebar.jsx
+++ src/Components/Store/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { FaTimes } from "react-icons/fa";
 import { Link, useSearchParams } from "react-router-dom";
 
 export function Sidebar(props) {
@@ -6,13 +7,19 @@ export function Sidebar(props) {
     const searchCategory = searchParams.get('category');
 
   return (
-    <div className="w-1/4">
+    <div className="w-1/4 sm:w-44 fixed">
       <h2 className="text-lg font-semibold mb-4">Catégories</h2>
       <ul>
         {categorys.map(category =>
-          <li key={category} className="mb-2">
+          <li key={category} className="mb-2 flex justify-between">
             <Link to={`/?category=${category}`} className="text-blue-500 hover:underline">{category}</Link>
-            {category == searchCategory ? (<Link to={'/'} className="bg-red-500 rounded-full px-3 py-1 ml-5 text-white">X</Link>):(<></>)}
+            {category == searchCategory ? (
+            <Link to={'/'} className="">
+              <FaTimes className="bg-red-500 rounded-full p-1 w-6 h-6"/>
+              </Link>
+            ):(
+            <></>
+            )}
           </li>)}
       </ul>
     </div>

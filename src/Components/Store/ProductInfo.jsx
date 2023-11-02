bace255e63555b1b3763e81ca6c383ebe518821b
Eric Sergueev <eric.sergueev@gmail.com>
2023-11-02 11:47:29 +0100

    feat : link product

--- src/Components/Store/ProductInfo.jsx
+++ src/Components/Store/ProductInfo.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import {  addProductToCart } from "../Cart/cart";
+import { addProductToCart } from "../Cart/cart";
 
 function truncateDescription(description, maxLength) {
     if (description.length > maxLength) {
@@ -15,11 +15,11 @@ export function ProductInfo(props) {
 
     return (
         <div className="flex flex-col justify-between bg-white p-4 shadow rounded">
-            <img className="w-full mb-2" src={image} alt={title} />
             <Link to={`/product/${id}`} >
+                <img className="w-full mb-2" src={image} alt={title} />
                 <h3 className="text-lg font-semibold mb-2">{title}</h3>
+                <p className="text-gray-600 mb-2">{truncatedDescription}</p>
             </Link>
-            <p className="text-gray-600 mb-2">{truncatedDescription}</p>
             <div className="flex items-baseline justify-between">
                 <span className="text-blue-500 font-bold">{price} $</span>
                 <button onClick={() => addProductToCart(id, image, price)} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
